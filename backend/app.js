9d00204001971237e22186389c07e9451fd62b48
Lucas PICHOLLET <Lucas@MacBook-Pro-de-Lucas.local>
2023-07-06 15:15:56 +0200

    ajout routes api

--- backend/app.js
+++ backend/app.js
@@ -13,4 +13,53 @@ app.use((req, res, next) => {
     res.json({ message: 'cette API est en cours de construction' });
 });
 
+/*
+app.post('/api/auth/signup', (req, res) => {
+    res.status(200).json({ message: string });
+});
+
+app.post('/api/auth/login', (req, res) => {
+    res.status(200).json({ userId: string, token: string });
+});
+
+app.get('/api/books', (req, res) => {
+    const books = [
+
+    ];
+    res.status(200).json(books);
+});
+
+app.get('/api/books/:id', (req, res) => {
+    const id = ;
+    res.status(200).json(id.book);
+});
+
+app.get('/api/books/bestrating', (req, res) => {
+    const bestrating = [
+
+    ];
+    res.status(200).json(bestrating);
+});
+
+app.post('/api/books', (req, res) => {
+    res.status(200).json({ message: string });
+});
+
+app.put('/api/books/:id', (req, res) => {
+    const id = ;
+    res.status(200).json({ message: string });
+});
+
+app.delete('/api/books/:id', (req, res) => {
+    const id = ;
+    res.status(200).json({ message: string });
+});
+
+app.post('/api/books/:id/rating', (req, res) => {
+    const id = ;
+    const rating = ;
+    res.status(200).json(id.book.rating);
+});
+*/
+
 module.exports = app;
