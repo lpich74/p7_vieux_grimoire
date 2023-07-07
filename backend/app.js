fa4366c36ba35a1301d35dd11271e043eef7c8c5
Lucas PICHOLLET <Lucas@MacBook-Pro-de-Lucas.local>
2023-07-07 17:05:26 +0200

    conneexion a MongoDB

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,18 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://lucaspichollet:Nq8BtFZbRWyG57U8@lucasp-cluster0.mscptrr.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const User = require('./models/User');
+//const Book = require('./models/Book');
+
+app.use(express.json());
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -9,15 +21,16 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.json({ message: 'cette API est en cours de construction' });
+app.post('/api/auth/signup', (req, res) => {
+    const user = new User({
+        ...req.body
+    });
+    user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
 });
 
 /*
-app.post('/api/auth/signup', (req, res) => {
-    res.status(200).json({ message: string });
-});
-
 app.post('/api/auth/login', (req, res) => {
     res.status(200).json({ userId: string, token: string });
 });
